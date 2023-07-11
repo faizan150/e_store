471642e95e19ac37e5a45b3874a69bf56a28be1a
Muhammad Faizan <faizi12570@gmail.com>
2023-07-11 19:27:23 +0500

    Some Logo setting

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 
-import logo from "/public/logo.webp";
+import logo from "../../../public/Logo.webp";
+
 import Image from "next/image";
 import Link from "next/link";
 import { ShoppingCart } from "lucide-react";
